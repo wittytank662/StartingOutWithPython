cf3011491439479df68e1e336987a901aca242a7
Sam <78057250+wittytank662@users.noreply.github.com>
2025-04-10 12:03:36 -0700

    Completing exercise 1 practicing printing.

---
+++ Chapter02/Exercises/e01PersonalInformation.py
@@ -0,0 +1,17 @@
+print("Your name is John Doe")
+print("You live at 123 Generic Street in Seattle Washington 98112")
+print("Your phone number is 555-555-5555")
+print("Your college major is computer engineering.")
+
+
+
+
+
+"""*************OUTPUT***************
+    
+Your name is John Doe
+You live at 123 Generic Street in Seattle Washington 98112
+Your phone number is 555-555-5555
+Your college major is computer engineering.
+    
+"""
