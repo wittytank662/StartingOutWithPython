de889694e100526632b4e3640da62f8de60fa5a5
Sam <78057250+wittytank662@users.noreply.github.com>
2025-04-25 12:46:31 -0700

    Completed the exercises for chapter 1 of starting out with python

---
+++ Chapter01/Exercises/e03convertDecimalToBinary.py
@@ -0,0 +1,4 @@
+print("11 in binary is 001011")
+print("65 in binary is 000001 000001")
+print("100 in binary is 000001 100100")
+print("255 in binary is 000011 111111")
---
+++ Chapter01/Exercises/e04convertBinaryToDecimal.py
@@ -0,0 +1,3 @@
+print("001101 in decimal is 13")
+print("001000 in decimal is 8")
+print("101011 in decimal is 43")
