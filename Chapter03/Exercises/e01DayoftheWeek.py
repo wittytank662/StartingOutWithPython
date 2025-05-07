dd6dd2c71d27f6c8a30a81f6bfded8f139a00b47
Sam <78057250+wittytank662@users.noreply.github.com>
2025-05-07 07:36:20 -0700

    Completed more exercises and examples

---
+++ Chapter03/Exercises/e01DayoftheWeek.py
@@ -0,0 +1,18 @@
+day = int(input("Choose a number 1-7: "))
+
+if day == 1:
+    print("The day is monday.")
+elif day == 2:
+    print("The day is tuesday.")
+elif day == 3:
+    print("The day is wednesday.")
+elif day == 4:
+    print("The day is thursday.")
+elif day == 5:
+    print("The day is friday.")
+elif day == 6:
+    print("The day is saturday.")
+elif day == 7:
+    print("The day is sunday.")
+else:
+    print("That number is not in the range of 1-7.")
---
+++ Chapter03/Exercises/e02AreasofRectangles.py
@@ -0,0 +1,17 @@
+# Rectangle area = len * wid
+
+rect1len = float(input("What is the length of the first rectangle? "))
+rect1wid = float(input("What is the width of the first rectangle? "))
+
+rect2len = float(input("What is the length of the second rectangle? "))
+rect2wid = float(input("What is the width of the second rectangle? "))
+
+rect1area = rect1len * rect1wid
+rect2area = rect2len * rect2wid
+
+if rect1area > rect2area:
+    print("The first rectangle")
+elif rect2area > rect1area:
+    print("The second rectangle has a larger area")
+else:
+    print("The rectangles have the same area.")
