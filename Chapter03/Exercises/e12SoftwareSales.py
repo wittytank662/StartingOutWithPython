456a4f58b0a5c469b135bf95875a4400a7908827
Sam <78057250+wittytank662@users.noreply.github.com>
2025-05-24 23:27:34 -0700

    didnt have much time only was able to complete one exercise

---
+++ Chapter03/Exercises/e12SoftwareSales.py
@@ -0,0 +1,15 @@
+quantity = int(input("How many packages did you buy? "))
+price = 99 * quantity
+
+
+if quantity >= 10 and quantity <= 19:
+    discount = price * 0.1
+elif quantity >= 20 and quantity <= 49:
+    discount = price * 0.2
+elif quantity >= 50 and quantity <= 99:
+    discount = price * 0.3
+elif quantity >= 100:
+    discount = price * 0.4
+    
+print(f'Your discount is: {discount}.')
+print(f'The total amount of the purchase is: {price - discount}.')
