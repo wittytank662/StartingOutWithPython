b9de617f59a7048a02aa94add405cfc0badfb4b1
Sam <78057250+wittytank662@users.noreply.github.com>
2025-05-26 22:57:30 -0700

    completed 1 exercise for textbook

---
+++ Chapter03/Exercises/e14BodyMassIndex.py
@@ -0,0 +1,11 @@
+weight = int(input("Enter your weight in lbs: "))
+height = int(input("Enter your height in inches: "))
+
+BMI = weight * (703/(height ** 2))
+
+if BMI < 18.5:
+    print("You are underweight.")
+if BMI >= 18.5 and BMI <= 25:
+    print("You have the optimal weight.")
+elif BMI > 25:
+    print("You are overweight.")
