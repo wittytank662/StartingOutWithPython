96a03f77a77b530b6a961a9b61b66b640ccfac85
Sam <78057250+wittytank662@users.noreply.github.com>
2025-05-30 18:29:21 -0700

    completed all chap 3 exercvies

---
+++ Chapter03/Exercises/e15TimeCalculator.py
@@ -0,0 +1,22 @@
+totalSeconds = int(input("Enter a number of seconds: "))
+
+days = 0
+hours = 0
+minutes = 0
+seconds = 0
+
+if totalSeconds >= 86400:
+    days = totalSeconds // 86400
+    totalSeconds = totalSeconds % 86400
+
+if totalSeconds >= 3600:
+    hours = totalSeconds // 3600
+    totalSeconds = totalSeconds % 3600
+
+if totalSeconds >= 60:
+    minutes = totalSeconds // 60
+    totalSeconds = totalSeconds % 60
+
+seconds = totalSeconds
+
+print(f'{days} day(s), {hours} hour(s), {minutes} minute(s), {seconds} second(s).')
