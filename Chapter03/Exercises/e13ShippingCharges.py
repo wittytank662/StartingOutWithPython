df7251f5a7cedbba5fe836497e27d5e6c02fae56
Sam <78057250+wittytank662@users.noreply.github.com>
2025-06-01 23:35:02 -0700

    added an output

--- Chapter03/Exercises/e13ShippingCharges.py
+++ Chapter03/Exercises/e13ShippingCharges.py
@@ -8,3 +8,13 @@ elif lbs > 6 and lbs <= 10:
     print(f"Your price is: {lbs * 4.00}")
 elif lbs > 10:
     print(f"Your price is: {lbs * 4.75}")
+    
+'''
+OUTPUT
+
+How much does your package weigh? 500
+Your price is: 2375.0
+
+How much does your package weigh? 200
+Your price is: 950.0
+'''
