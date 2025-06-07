d827ebdda5c1f6270090ca0487d78d1ebd6d84b0
Sam <78057250+wittytank662@users.noreply.github.com>
2025-06-06 23:33:07 -0700

    added output

--- Chapter02/Exercises/e03LandCalculation.py
+++ Chapter02/Exercises/e03LandCalculation.py
@@ -3,3 +3,13 @@ totSqFt = int(input("Please enter the total square feet of a plot of land. "))
 acres = totSqFt / 43560
 
 print("The amount of acres in your plot of land are:", acres)
+
+'''
+OUTPUT
+
+Please enter the total square feet of a plot of land. 5000
+The amount of acres in your plot of land are: 0.1147842056932966
+
+Please enter the total square feet of a plot of land. 60000000
+The amount of acres in your plot of land are: 1377.4104683195592
+'''
