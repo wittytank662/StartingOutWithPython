adeec2abcfdc4639db5431a20fec9f5af62cb9df
Sam <78057250+wittytank662@users.noreply.github.com>
2025-06-07 23:34:41 -0700

    added output

--- Chapter02/Exercises/e04TotalPurchase.py
+++ Chapter02/Exercises/e04TotalPurchase.py
@@ -14,3 +14,15 @@ total = subTotal + salesTax
 
 print("Your subtotal is:", subTotal)
 print("Your total is:", total)
+
+'''
+OUTPUT
+
+What is the price of your first item? 99.99
+What is the price of your second item? 24.99
+What is the price of your third item? 14.99
+What is the price of your fourth item? 5.49
+What is the price of your fifth item? 6.99
+Your subtotal is: 152.45000000000002
+Your total is: 163.12150000000003
+'''
