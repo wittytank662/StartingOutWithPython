d4def35e83031c85dfd58014815b05863309c5df
Sam <78057250+wittytank662@users.noreply.github.com>
2025-06-15 23:47:37 -0700

    completed a exercise

---
+++ Chapter04/ExampleCode/p4.04simple_loop1.py
@@ -0,0 +1,17 @@
+# This program demonstrates a simple for loop
+# that uses a list of numbers.
+
+print('I will display the numbers 1 through 5.')
+for num in [1, 2, 3, 4, 5]:
+    print(num)
+
+'''
+OUTPUT
+
+I will display the numbers 1 through 5.
+1
+2
+3
+4
+5
+'''
