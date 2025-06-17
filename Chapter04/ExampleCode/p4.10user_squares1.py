e5e47ebdf58656c639659d6807c6e6d3f0966299
Sam <78057250+wittytank662@users.noreply.github.com>
2025-06-17 14:37:10 -0700

    completed an exercise

---
+++ Chapter04/ExampleCode/p4.10user_squares1.py
@@ -0,0 +1,31 @@
+# This program uses a loop to display a
+# table of numbers and their squares.
+
+# Get the ending limit.
+print('This program displays a list of numbers')
+print('(starting at 1) and their squares.')
+end = int(input('How high should I go?'))
+
+# Print the table headings.
+print()
+print('Number\tSquare')
+print('--------------')
+
+# Print the numbers and their squares.
+for number in range(1, end + 1):
+    square = number**2
+    print(number, '\t', square)
+    
+'''
+OUTPUT
+
+How high should I go?5
+
+Number  Square
+--------------
+1        1
+2        4
+3        9
+4        16
+5        25
+'''
